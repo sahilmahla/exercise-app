799c28c15844feb4ff26eec0f710ea5d3348959c
Sahil Mahla <sahilmahla1697@gmail.com>
2022-09-02 03:20:15 +0530

    implemented home page banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import { Box } from '@mui/material'
+import HeroBanner from '../components/HeroBanner'
+import SearchExercises from '../components/SearchExercises'
+import Exercises from '../components/Exercises'
 const Home = () => {
   return (
-    <div>Hello</div>
+    <Box>
+        <HeroBanner />
+        <SearchExercises />
+        <Exercises />
+    </Box>
   )
 }
 
