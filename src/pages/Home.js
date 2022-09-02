d60c0f89d68d0c663dffcf0418f94324db7fdb27
Sahil Mahla <sahilmahla1697@gmail.com>
2022-09-02 12:03:41 +0530

    implemented categories search categories ui ,  fetch data from api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,13 @@ import HeroBanner from '../components/HeroBanner'
 import SearchExercises from '../components/SearchExercises'
 import Exercises from '../components/Exercises'
 const Home = () => {
+    const [exercises, setExercises] = useState([]);
+    const [bodyPart, setBodyPart] = useState('all');
   return (
     <Box>
         <HeroBanner />
-        <SearchExercises />
-        <Exercises />
+        <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+        <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
     </Box>
   )
 }
