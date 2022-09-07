f22f574522efbbbfa8722577d567be346ace8d6e
Ujjwal Garg <74059664+BRUTAlVision@users.noreply.github.com>
2022-09-07 14:00:34 +0530

    adding search exercise functionalities

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ import Logo from '../assets/images/Logo.png';
 const Navbar = () => (
   <Stack direction="row" justifyContent="space-around" sx={{ gap: { sm: '123px', xs: '40px' }, mt: { sm: '32px', xs: '20px' }, justifyContent: 'none' }} px="20px">
     <Link to="/">
-      <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0px 20px' }} />
+      <img src={Logo} alt="logo" style={{ width: '48px', height: '48px', margin: '0px 15px' }} />
     </Link>
     <Stack
       direction="row"
-      gap="40px"
+      gap="20px"
       fontFamily="Alegreya"
       fontSize="24px"
       alignItems="flex-end"
